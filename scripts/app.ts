520f63ed0aaed283a7c020c560421045c84b1b2c
john-lay <john_lay@hotmail.co.uk>
2019-07-29 20:44:43 +0100

    Refactor boilerplate phaser js to typescript

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,18 +1,23 @@
-// app.ts
 require('phaser');
-export class Hero {
-    id: number;
-    name: string;
 
-    constructor(name: string) {
-        this.name = name;
+let preload = () => {
+}
+
+let create = () => {
+}
+
+let update = () => {
+}
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
+};
 
-    myName()
-    {
-        return this.name;
-    }
-  }
-
-  let hero = new Hero('Shotgun');
-  console.log(hero.myName());
+const game = new Phaser.Game(config);
