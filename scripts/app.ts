12def88a58ddbd4e69ce1d95bee670202f455a67
john-lay <john_lay@hotmail.co.uk>
2019-08-01 21:04:06 +0100

    Complete parts 2 and 3 of the Phaser tutorial

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,12 +1,21 @@
 require('phaser');
 
-let preload = () => {
+function preload() {    
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
-let create = () => {
+function create() {
+    this.add.image(400, 300, 'sky');
 }
 
-let update = () => {
+function update() {
 }
 
 const config: Phaser.Types.Core.GameConfig = {
