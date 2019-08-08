4c581a1dcc612063a193f4a398abd2d9a0d230cc
john-lay <john_lay@hotmail.co.uk>
2019-08-08 20:37:26 +0100

    Add part 4 of phaser tutorial

--- scripts/app.ts
+++ scripts/app.ts
@@ -5,14 +5,19 @@ function preload() {
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
     this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update() {
@@ -22,6 +27,13 @@ const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -29,4 +41,5 @@ const config: Phaser.Types.Core.GameConfig = {
     }
 };
 
+let platforms: Phaser.Physics.Arcade.StaticGroup;
 const game = new Phaser.Game(config);
